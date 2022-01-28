b06bc177792cbfa8d28d9644fa1cdbd74eaa711c
Sebastien Bihorel <sb.pmlab@gmail.com>
2022-01-28 20:10:06 +0000

    version 1.0-8

--- inst/doc/manual.R
+++ inst/doc/manual.R
@@ -1,16 +1,16 @@
-### R code from vignette source 'optimsimplex_manual.Rnw'
-### Encoding: UTF-8
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
 
-###################################################
-### code chunk number 1: optimsimplex_manual.Rnw:46-48
-###################################################
+## ----echo = FALSE, message = FALSE--------------------------------------------
 require(optimsimplex)
-options(width=70)
 
+## ----eval=FALSE---------------------------------------------------------------
+#     myfunction <- function(x, this){
+#       ...
+#       return(list(f=f,this=this))
+#     }
 
-###################################################
-### code chunk number 2: optimsimplex_manual.Rnw:125-132
-###################################################
+## -----------------------------------------------------------------------------
 coords <- matrix(c(0,1,0,0,0,1),ncol=2)
 tmp <- optimsimplex(coords=coords)
 s1 <- tmp$newobj
@@ -19,10 +19,7 @@ optimsimplex.getallx(s1)
 optimsimplex.getn(s1)
 optimsimplex.getnbve(s1)
 
-
-###################################################
-### code chunk number 3: optimsimplex_manual.Rnw:143-164
-###################################################
+## -----------------------------------------------------------------------------
 rosenbrock <- function(x){
   y <- 100*(x[2]-x[1]^2)^2+(1-x[1])^2
 }
@@ -44,5 +41,4 @@ tmp$newobj
 tmp$data
 
 cat(sprintf("Function evaluations: %d\n",tmp$data$nb))
-
 
